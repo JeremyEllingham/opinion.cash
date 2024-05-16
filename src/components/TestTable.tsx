d3698a02b41732fb7c88c27a79d87110fa3f45f9
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-05-16 11:09:59 +0200

    Check page.

---
+++ src/components/TestTable.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const TestTable = () => {
+  return (
+    <div>
+      <p>Test table</p>
+      <table>
+        <thead>
+          <tr>
+            <th />
+            <th>Uninterested</th>
+            <th>Interested</th>
+            <th>Ideation</th>
+            <th>Research</th>
+            <th>Feedback</th>
+            <th>Iteration</th>
+            <th>Flawed</th>
+            <th>Draft Proposal</th>
+            <th>Finished</th>
+            <th>Endorsed</th>
+            <th>Locked In</th>
+            <th>Implemented</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>ABLA</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>VM Limits</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+
+          </tr>
+          <tr>
+            <td>UTXO Committments</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default TestTable
---
+++ src/pages/chips.tsx
@@ -0,0 +1,10 @@
+import Table from "../components/Table"
+import TestTable from "../components/TestTable"
+
+export default function CHIPs() {
+  return <div><div>CHIPs</div>
+  <Table />
+  <hr />
+  <TestTable />
+  </div>
+}
